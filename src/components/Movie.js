c35d4228b03670be07bbade79170c2ba0a97ead1
kluz116 <engallanmusembya@gamil.com>
2019-07-18 13:44:56 +0300

    Movie search app react Js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import SearchMovie from './SearchMovie';
+import NavBar from './NavBar';
 
 export default class Movie extends Component {
     render() {
         return (
             <div>
-                  
+                  <NavBar></NavBar>
                   <SearchMovie></SearchMovie>
             </div>
         )
